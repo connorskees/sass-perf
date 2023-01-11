8440f5f36e3d617d4bff70e3c9fb751c5375e435
connorskees <connor1skees@gmail.com>
2023-01-11 15:45:05 +0000

    add other benches

--- bench.sh
+++ bench.sh
@@ -2,16 +2,16 @@ bench() {
     hyperfine "./dart-sass/sass $1 $2" "grass $1 $2" "./sassc/bin/sassc $1 $2" --warmup 5
 }
 
-# bench bootstrap/scss/bootstrap.scss
-# bench bulma/bulma.sass
-# bench small-plain.scss
-# bench large-plain.scss
-# bench preceding-sparse-extend.scss
-# bench following-sparse-extend.scss
-# bench preceding-dense-extend.scss
-# bench following-dense-extend.scss
-# bench single-bootstrap.scss
-# bench large-bootstrap.scss
-# bench a11ycolor.scss
-# bench duomo.scss
+bench bootstrap/scss/bootstrap.scss
+bench bulma/bulma.sass
+bench small-plain.scss
+bench large-plain.scss
+bench preceding-sparse-extend.scss
+bench following-sparse-extend.scss
+bench preceding-dense-extend.scss
+bench following-dense-extend.scss
+bench single-bootstrap.scss
+bench large-bootstrap.scss
+bench a11ycolor.scss
+bench duomo.scss
 bench carbon.scss -Iibm-cloud-cognitive/node_modules
